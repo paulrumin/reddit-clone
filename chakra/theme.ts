41d3d7d980e5e3a7820b2994f7c2cfac4f5888fb
paulrumin <pasha@jedo.localdomain>
2023-03-23 13:39:49 -0700

    Chakra init

---
+++ chakra/theme.ts
@@ -0,0 +1,27 @@
+// 1. Import `extendTheme`
+import "@fontsource/open-sans/300.css"
+import "@fontsource/open-sans/500.css"
+import "@fontsource/open-sans/700.css"
+
+import { extendTheme } from "@chakra-ui/react"
+
+// 2. Call `extendTheme` and pass your custom values
+export const theme = extendTheme({
+  colors: {
+    brand: {
+      100: "#ff3c00",
+    },
+  },
+  fonts:{
+    body: 'Open Sans, san-serif'
+  },
+  styles:{
+    global: () => ({
+      body: {
+        bg: "gray.200", 
+      },
+    }),
+  }
+})
+
+

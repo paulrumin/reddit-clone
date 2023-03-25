dbbac8ce0aee8ae24b84bf5b5fe116d0772a08b4
paulrumin <pasha@jedo.localdomain>
2023-03-25 12:56:41 -0700

    Navbar

--- chakra/theme.ts
+++ chakra/theme.ts
@@ -21,6 +21,9 @@ export const theme = extendTheme({
         bg: "gray.200", 
       },
     }),
+  },
+  components:{
+
   }
 })
 
---
+++ components/Navbar/Searchinput.tsx
@@ -0,0 +1,13 @@
+import { Flex, Image } from "@chakra-ui/react"
+import React from "react";
+
+const Searchinput:React.FC = () => {
+
+      return(
+        <Flex>
+         
+
+        </Flex>
+      );
+}
+export default Searchinput
